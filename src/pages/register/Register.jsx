e64f77bd0c13596dc9ab6e6fcb6e9a53818a991c
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-16 12:27:44 +0800

    Register Page UI

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -2,7 +2,36 @@ import './register.scss'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='register'>
+      <div className="card">
+				<div className="left">
+          <h1>Register</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Name" />
+            <button type="button" 
+							// onClick={handleLogin}
+						>Register</button>
+          </form>
+				</div>
+				<div className="right">
+					<h1>
+						lama social
+					</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+							alias totam numquam ipsa exercitationem dignissimos, error nam,
+							consequatur.
+					</p>
+					<span>
+						Do you have an account?
+					</span>	
+					<button type="button">Login</button>
+				</div>
+			</div>
+    </div>
   )
 }
 
