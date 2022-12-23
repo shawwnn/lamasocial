40a8d3469f7f750b4d9ba502dd4abfd9c0549a51
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-12-23 22:33:52 +0800

    Profile Page UI

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import "./post.scss"
+import "./post.scss";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
 import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
@@ -7,21 +6,25 @@ import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
+import { useState } from "react";
 
-const Post = ( { post } ) => {
-  const [isCommentOpen, setIsCommentOpen] = useState(false)
+const Post = ({ post }) => {
+  const [commentOpen, setCommentOpen] = useState(false);
 
-  // temporary
+  //TEMPORARY
   const liked = false;
 
   return (
     <div className="post">
       <div className="container">
         <div className="user">
-          <div className="user-info">
+          <div className="userInfo">
             <img src={post.profilePic} alt="" />
             <div className="details">
-              <Link to={`/profile${post.userId}`} style={{textDecoration: "none", color: "inherit"}}>
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
                 <span className="name">{post.name}</span>
               </Link>
               <span className="date">1 min ago</span>
@@ -36,9 +39,9 @@ const Post = ( { post } ) => {
         <div className="info">
           <div className="item">
             {liked ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
-            12 likes
+            12 Likes
           </div>
-          <div className="item" onClick={() => setIsCommentOpen(!isCommentOpen)}>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
             <TextsmsOutlinedIcon />
             12 Comments
           </div>
@@ -47,11 +50,10 @@ const Post = ( { post } ) => {
             Share
           </div>
         </div>
-
-        {isCommentOpen && <Comments />}
+        {commentOpen && <Comments />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
