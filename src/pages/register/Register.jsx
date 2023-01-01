a6c401e6b708513765abaa2261f33712be06d61f
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-01-01 12:47:21 +0800

    important: connected auth register backend to frontend

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,51 @@
+import axios from 'axios'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import './register.scss'
 
 const Register = () => {
+	const [inputs, setInputs] = useState({
+		username: "",
+		email: "",
+		password: "",
+		name: "",
+		
+	})
+
+	const [err, setErr] = useState(null)
+
+	const handleChange = (e) => {
+		setInputs((prev) => ({...prev, [e.target.name]: e.target.value }))
+	}
+
+	// console.log(inputs);
+
+	const handleLogin = async (e) => {
+		e.preventDefault()
+
+		try {
+			await axios.post("http://localhost:8800/api/auth/register", inputs)
+		} catch (err) {
+			setErr(err)
+		}
+	}
+
+	// console.log(err)
+
   return (
     <div className='register'>
       <div className="card">
 				<div className="left">
           <h1>Register</h1>
           <form>
-            <input type="text" placeholder="Username" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <input type="text" placeholder="Name" />
-            <button type="button" 
-							// onClick={handleLogin}
+            <input type="text" placeholder="Username" name="username" onChange={handleChange} />
+            <input type="email" placeholder="Email" name="email" onChange={handleChange} />
+            <input type="password" placeholder="Password" name="password" onChange={handleChange} />
+            <input type="text" placeholder="Name" name="name" onChange={handleChange} />
+            
+						{/* {err && err} */}
+						<button type="button" 
+							onClick={handleLogin}
 						>Register</button>
           </form>
 				</div>
