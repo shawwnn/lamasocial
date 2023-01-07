4237b0db955a2f1e9653ab5de0d78f761e662de0
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-01-07 11:20:27 +0800

    important: Add Post functionality in MySQL React Add New Post to MySQL Database React-Query Mutation Tutorial

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -2,12 +2,42 @@ import "./share.scss";
 import Image from "../../assets/img.png";
 import Map from "../../assets/map.png";
 import Friend from "../../assets/friend.png";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../context/authContext";
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { makeRequest } from "../../axios.js"
 
 const Share = () => {
+  const [file, setFile] = useState(null)
+  const [desc, setDesc] = useState("")
 
   const {currentUser} = useContext(AuthContext)
+
+  const queryClient = new useQueryClient()
+
+  // Queries
+  // const query = useQuery({ queryKey: ['todos'], queryFn: getTodos })
+
+  const addSharePost = (newPost) => {
+    return makeRequest.post("/posts", newPost)
+  }
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: addSharePost,
+    onSuccess: () => {
+      // Invalidate and refetch
+      // queryClient.invalidateQueries({ queryKey: ['todos'] })
+      queryClient.invalidateQueries(["posts"])
+    },
+  })
+
+  const handleShare = (e) => {
+    e.preventDefault()
+
+    mutation.mutate({desc})
+  }
+
   return (
     <div className="share">
       <div className="container">
@@ -16,12 +46,16 @@ const Share = () => {
             src={currentUser.profilePic}
             alt=""
           />
-          <input type="text" placeholder={`What's on your mind ${currentUser.name}?`} />
+          <input type="text" 
+            placeholder={`What's on your mind ${currentUser.name}?`} 
+            onChange={(e) => setDesc(e.target.value)}/>
         </div>
         <hr />
         <div className="bottom">
           <div className="left">
-            <input type="file" id="file" style={{display:"none"}} />
+            <input type="file" id="file" style={{display:"none"}} 
+              onChange={(e) => setFile(e.target.files[0])}  
+            />
             <label htmlFor="file">
               <div className="item">
                 <img src={Image} alt="" />
@@ -38,7 +72,7 @@ const Share = () => {
             </div>
           </div>
           <div className="right">
-            <button>Share</button>
+            <button onClick={handleShare}>Share</button>
           </div>
         </div>
       </div>
