1b676527ee0e840cf3ca21d272f94d7a0e19703e
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-01-10 18:43:09 +0800

    Social Media App Add a New Comment (Client)

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,11 +1,13 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "./comments.scss"
 import { AuthContext } from "../../context/authContext"
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { makeRequest } from "../../axios";
 import moment from "moment"
 
 const Comments = ({ postId }) => {
+  const [desc, setDesc] = useState("")
+
   const { currentUser } = useContext(AuthContext);
 
   const { isLoading, error, data } = useQuery(['comments'], () => 
@@ -13,8 +15,6 @@ const Comments = ({ postId }) => {
       return res.data;
     })
   )
-
-  console.log(data)
 
   // const comments = [
   //   {
@@ -35,18 +35,48 @@ const Comments = ({ postId }) => {
   //   },
   // ];
 
+  const queryClient = new useQueryClient()
+
+  // Queries
+  // const query = useQuery({ queryKey: ['todos'], queryFn: getTodos })
+
+  const addComment = (newComment) => {
+    return makeRequest.post("/comments", newComment)
+  }
+
+  // Mutations
+  const mutation = useMutation({
+    mutationFn: addComment,
+    onSuccess: () => {
+      // Invalidate and refetch
+      // queryClient.invalidateQueries({ queryKey: ['todos'] })
+      queryClient.invalidateQueries(["comments"])
+    },
+  })
+
+  const handleAddComment = async (e) => {
+    e.preventDefault()
+
+    mutation.mutate({ desc, postId })
+
+    setDesc("")
+  }
+
   return (
     <div className="comments">
       <div className="write">
         <img src={currentUser.profilePic} alt="" />
-        <input type="text" placeholder="write a comment" />
-        <button type="">Send</button>
+        <input type="text" placeholder="write a comment" 
+          value={desc}
+          onChange={(e) => setDesc(e.target.value)} 
+        />
+        <button type="button" onClick={handleAddComment}>Send</button>
       </div>
       {isLoading 
         ? "loading" 
         : data.map(comment => (
         <div className="comment">
-          <img src={comment.profilePicture} alt="" />
+          <img src={comment.profilePic} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
