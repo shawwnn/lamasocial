69228992ac2d8da12268149f96decc1215cb2b3c
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-01-10 17:58:13 +0800

    Social Media App Fetch Comments (client)

--- src/components/comments/Comments.jsx
+++ src/components/comments/Comments.jsx
@@ -1,28 +1,39 @@
 import { useContext } from "react";
 import "./comments.scss"
 import { AuthContext } from "../../context/authContext"
+import { useQuery } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import moment from "moment"
 
-const Comments = () => {
+const Comments = ({ postId }) => {
   const { currentUser } = useContext(AuthContext);
 
-  const comments = [
-    {
-      id: 1,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "John Doe",
-      userId: 1,
-      profilePicture:
-        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    },
-    {
-      id: 2,
-      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
-      name: "Jane Doe",
-      userId: 2,
-      profilePicture:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
-    },
-  ];
+  const { isLoading, error, data } = useQuery(['comments'], () => 
+    makeRequest.get("/comments?postId="+postId).then((res) => {
+      return res.data;
+    })
+  )
+
+  console.log(data)
+
+  // const comments = [
+  //   {
+  //     id: 1,
+  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+  //     name: "John Doe",
+  //     userId: 1,
+  //     profilePicture:
+  //       "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  //   },
+  //   {
+  //     id: 2,
+  //     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem nequeaspernatur ullam aperiam",
+  //     name: "Jane Doe",
+  //     userId: 2,
+  //     profilePicture:
+  //       "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  //   },
+  // ];
 
   return (
     <div className="comments">
@@ -31,14 +42,16 @@ const Comments = () => {
         <input type="text" placeholder="write a comment" />
         <button type="">Send</button>
       </div>
-      {comments.map(comment => (
+      {isLoading 
+        ? "loading" 
+        : data.map(comment => (
         <div className="comment">
           <img src={comment.profilePicture} alt="" />
           <div className="info">
             <span>{comment.name}</span>
             <p>{comment.desc}</p>
           </div>
-          <div className="date">1 hour ago</div>
+          <div className="date">{moment(comment.createAt).fromNow()}</div>
         </div>
       ))}
     </div>
