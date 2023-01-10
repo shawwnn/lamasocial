db0f95264aee9f55c3e6f893e734450ec084e563
Shawn Lapuz <shangel.lapuz@gmail.com>
2023-01-10 10:47:48 +0800

    React Node.js How to Upload File to Server (client side)

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -11,6 +11,17 @@ const Share = () => {
   const [file, setFile] = useState(null)
   const [desc, setDesc] = useState("")
 
+  const upload = async () => {
+    try {
+      const formData = new FormData();
+      formData.append("file", file);
+      const res = await makeRequest.post("/upload", formData);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const {currentUser} = useContext(AuthContext)
 
   const queryClient = new useQueryClient()
@@ -32,23 +43,40 @@ const Share = () => {
     },
   })
 
-  const handleShare = (e) => {
+  const handleShare = async (e) => {
     e.preventDefault()
 
-    mutation.mutate({desc})
+    let imgUrl = ""
+
+    // console.log(file);
+    // return
+
+    if(file) imgUrl = await upload()
+
+    mutation.mutate({ desc, img: imgUrl})
+
+    setDesc("")
+    setFile(null)
   }
 
   return (
     <div className="share">
       <div className="container">
         <div className="top">
-          <img
-            src={currentUser.profilePic}
-            alt=""
-          />
-          <input type="text" 
-            placeholder={`What's on your mind ${currentUser.name}?`} 
-            onChange={(e) => setDesc(e.target.value)}/>
+          <div className="left">
+            <img
+              src={currentUser.profilePic}
+              alt=""
+            />
+            <input type="text" 
+              placeholder={`What's on your mind ${currentUser.name}?`} 
+              onChange={(e) => setDesc(e.target.value)}
+              value={desc}
+            />
+          </div>
+          <div className="right">
+            {file && <img className="file" alt="" src={URL.createObjectURL(file)} />}
+          </div>
         </div>
         <hr />
         <div className="bottom">
